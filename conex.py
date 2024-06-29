4781715e3cdc78c9b582f4f274e51d26a3ef5be8
Kleyton Romão <86174204+KleytonRomao@users.noreply.github.com>
2024-06-29 03:21:04 +0000

    0.3

--- conex.py
+++ conex.py
@@ -9,11 +9,12 @@ class Cones:
             
         for proxy in data:
             # Verificar se as chaves 'IP' e 'Porta' existem para este proxy
-            if "IP" in proxy and "Porta" in proxy:
+            if "IP" in proxy and "Port" in proxy:
                 ip_adrss = proxy['IP']
-                porta_addrss = proxy['Porta']
+                porta_addrss = proxy['Port']
                 command = f"nc -vz {ip_adrss} {porta_addrss}"
                 os.system(command)
             else:
                 print("Error")
 
+Cones.con()
